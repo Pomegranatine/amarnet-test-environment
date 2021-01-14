5a7352f432ed642d22e7ab9a93188f48976e0b34
Pomegranatine <khosbayar@customaware.com>
2021-01-14 15:42:09 +0800

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [{path:'home', component: HomeComponent},
-{path: 'server', component: ServersComponent, children: [
-{path: ':id', component: ServerComponent},
-{path: ':id/edit', component: EditServerComponent}
-]},
-{path: 'user', component: UserComponent},
-{path: '' , redirectTo: '/home' ,pathMatch: 'full'},
-{path: '**', component: NoPageComponent}]
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
