2912520eddae4cba0d00fa182b8364e9fd0b8cfd
Pomegranatine <khosbayar@customaware.com>
2021-01-18 10:02:21 +0800

    2 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HomeComponent } from "./home/home.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { WhyUsComponent } from './why-us/why-us.component';
-import { HelpComponent } from './help/help.component';
-import { PriceComponent } from './price/price.component';
-import { ProductsComponent } from './products/products.component';
-import { Services/authService } from './services/auth.service';
+import { WhyUsComponent } from "./why-us/why-us.component";
+import { HelpComponent } from "./help/help.component";
+import { PriceComponent } from "./price/price.component";
+import { ProductsComponent } from "./products/products.component";
+
+import { AngularFireModule } from "@angular/fire";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HelloComponent, HomeComponent, WhyUsComponent, HelpComponent, PriceComponent, ProductsComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, AngularFireModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    WhyUsComponent,
+    HelpComponent,
+    PriceComponent,
+    ProductsComponent
+  ],
   bootstrap: [AppComponent],
-  providers: [Services/authService]
+  providers: []
 })
 export class AppModule {}
--- src/app/services/auth.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Services/authService {
-
-  constructor() { }
-
-}
